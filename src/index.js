66c4a32f5a5c56ccb243e675fb9973de732d09fe
Leonides <xxluckyleoxx1@gmail.com>
2021-04-12 14:22:31 -0400

    almost working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+
+function fetchDogs(){
+    return fetch("https://dog.ceo/api/breeds/image/random/4").then(resp => 
+    resp.json()).then(json => 
+        renderDogs(json))
+
+}
+function renderDogs(dogs){
+    let dogList = document.querySelector("ul#dog-breeds")
+    dogs.forEach(dog => {
+        let dogLi = document.createElement("li")
+        let dogImg = document.createElement('img')
+        dogImg.src = dog.message
+        dogLi.innerHTML = dogImg
+        dogList.append(dogLi)
+    })
+}
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchDogs()
+})
