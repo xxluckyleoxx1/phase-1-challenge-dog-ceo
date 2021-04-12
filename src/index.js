b44b5c3e8a92408243fc14ace0888edaa7bd10da
Michael Primavera <pastasauce59@gmail.com>
2021-04-12 14:57:30 -0400

    Completed Challange1. Working on Challenge 2.

--- src/index.js
+++ src/index.js
@@ -2,20 +2,35 @@ console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
 
 function fetchDogs(){
-    return fetch("https://dog.ceo/api/breeds/image/random/4").then(resp => 
-    resp.json()).then(json => 
-        renderDogs(json))
+    return fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(resp => resp.json())
+    .then(json => renderDogs(json.message))
 
 }
+
+function fetchAllBreeds(){
+    return fetch('https://dog.ceo/api/breeds/list/all')
+    .then(resp => resp.json())
+    .then(json => console.log(json.message))
+}
+
 function renderDogs(dogs){
-    let dogList = document.querySelector("ul#dog-breeds")
+    let dogList = document.querySelector("div#dog-image-container")
     dogs.forEach(dog => {
         let dogLi = document.createElement("li")
         let dogImg = document.createElement('img')
-        dogImg.src = dog.message
-        dogLi.innerHTML = dogImg
+        dogImg.src = dog
+        dogList.append(dogImg)
         dogList.append(dogLi)
     })
+
+    function renderAllBreeds(breeds){
+        let breedList = document.querySelector('ul#dog-breeds')
+        breeds.forEach(breed => {
+            let breedLi = document.createElement('li')
+            
+        })
+    }
 }
 
 
